a8c8340badb026a9bbfe9b32527f326caa18dbdf
Goatifi <daniel.ohandjanian.846@accesstomusic.ac.uk>
2024-01-25 15:09:02 +0000

    attempted to fix a bug

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,21 @@
 import React from "react";
 import CardItem from "../CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
     <div className="cards">
       <h1>Check out these EPIC Destinations!</h1>
+
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem />
+            <CardItem
+            // src="C:/Users/daniel.ohandjanian.8/Documents/GitHub/gibJohn/public/images/img-9.jpg"
+            // text="Explore the hidden waterfall deep inside the Amazon Jungle"
+            // label="Adventure"
+            // path="/services"
+            />
           </ul>
         </div>
       </div>
