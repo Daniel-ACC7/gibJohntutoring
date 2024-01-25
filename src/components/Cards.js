17fe3ee3c4829c48c20578d669bcde7e3237311d
Goatifi <daniel.ohandjanian.846@accesstomusic.ac.uk>
2024-01-25 16:51:15 +0000

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,6 @@ function Cards() {
   return (
     <div className="cards">
       <h1>Check out these EPIC Destinations!</h1>
-
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
