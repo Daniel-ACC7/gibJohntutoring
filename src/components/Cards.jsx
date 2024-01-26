fa54dd37766e1026d92bf8c39a716f15bb0a5493
Goatifi <daniel.ohandjanian.846@accesstomusic.ac.uk>
2024-01-26 11:23:59 +0000

    Adding Image Request

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,10 +10,16 @@ function Cards() {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-            // src="C:/Users/daniel.ohandjanian.8/Documents/GitHub/gibJohn/public/images/img-9.jpg"
-            // text="Explore the hidden waterfall deep inside the Amazon Jungle"
-            // label="Adventure"
-            // path="/services"
+              src="images/img-9.jpg"
+              text="Explore the hidden waterfall deep inside the Amazon Jungle"
+              label="Adventure"
+              path="/services"
+            />
+            <CardItem
+              src="public\images\img-2.jpg"
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Luxury"
+              path="/services"
             />
           </ul>
         </div>
