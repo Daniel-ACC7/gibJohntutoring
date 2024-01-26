a05e79dad21c95f88370b1382d7a2b1fad7088e4
Goatifi <daniel.ohandjanian.846@accesstomusic.ac.uk>
2024-01-26 14:56:18 +0000

    Removing unnecessary images and cleaning up sign up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,11 +64,11 @@ function Navbar() {
                 className="nav-links-mobile"
                 onClick={closeMobileMenu}
               >
-                Sign Up
+                Sign In
               </Link>
             </li>
           </ul>
-          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+          {button && <Button buttonStyle="btn--outline">SIGN IN</Button>}
         </div>
       </nav>
     </>
