7cf6ac2a93890e865633ada3d6cbb1e6b7c15d86
Goatifi <112864958+Daniel-ACC7@users.noreply.github.com>
2024-01-27 22:52:56 +0000

    making backend

--- frontend/src/components/pages/LogIn.jsx
+++ frontend/src/components/pages/LogIn.jsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Validation from "../LoginValidation";
 
 function LogIn() {
-    return (
-      <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
-          <div className="bg-white p-3 rounded w-25">
-              <form action="">
-                  <div className="mb-3">
-                      <label htmlFor="email"><strong>Email</strong></label>
-                      <input type="email" placeholder="Enter Email" className="form-control rounded-0"/>
-                  </div>
-                  <div className="mb-3">
-                      <label htmlFor="password"><strong>Password</strong></label>
-                      <input type="password" placeholder="Enter Password" className="form-control rounded-0"/>
-                  </div>
-                  <button className="btn btn-success w-100 rounded-0"><strong>Log in</strong></button>
-                  <p>You agree to our terms and conditions upon sign up</p>
-                  <Link to="/signup" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"><strong>Create Account</strong></Link>
-              </form>
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleInput = (event) => {
+    setValues((prev) => ({
+      ...prev,
+      [event.target.name]: [event.target.value],
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(Validation(values));
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign-In</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              onChange={handleInput}
+              className="form-control rounded-0"
+            />
+            {errors.email && (
+              <span className="text-danger"> {errors.email}</span>
+            )}
           </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              onChange={handleInput}
+              className="form-control rounded-0"
+            />
+            {errors.password && (
+              <span className="text-danger"> {errors.password}</span>
+            )}
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            <strong>Sign In</strong>
+          </button>
+          <p>By signing in you agree to our Privacy Policy</p>
+          <p>Don't have an account? Create one instead:</p>
+          <Link
+            to="/signup"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            <strong>Create Account</strong>
+          </Link>
+        </form>
       </div>
-    )
-  }
-  
-  export default LogIn;
+    </div>
+  );
+}
+
+export default LogIn;
